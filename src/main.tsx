d99f5a24f840510a14595cffdb6dd496c59b4b32
kromenzi <kromenzi@gmail.com>
2026-08-25 08:27:31 +0300

    Apply balanced light and dark theme contrast

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./dark-text-contrast.css";
+import "./theme-contrast.css";
 import "./ncr-preview-fix.css";
 import "./sidebar-layout-fix.css";
 import { ErrorBoundary } from "@/components/error-boundary";
