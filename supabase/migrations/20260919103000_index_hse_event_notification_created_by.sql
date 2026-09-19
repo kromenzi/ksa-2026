32a424ab8c51ff10e6c4557a93295d4c8a5f90c5
kromenzi <kromenzi@gmail.com>
2026-09-19 18:25:58 +0300

    chore: remove superseded migration timestamp

--- supabase/migrations/20260919103000_index_hse_event_notification_created_by.sql
+++
@@ -1,2 +0,0 @@
-create index if not exists idx_hse_events_created_by on public.hse_events(created_by);
-create index if not exists idx_notification_outbox_created_by on public.notification_outbox(created_by);
